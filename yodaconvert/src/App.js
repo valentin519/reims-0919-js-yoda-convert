de5b594df36488c5352c31a09e6323510f0e0a5b
pablo <pmo83.mp@gmail.com>
2019-12-18 16:51:26 +0100

    css button improve

--- yodaconvert/src/App.js
+++ yodaconvert/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Converter from './components/Converter'
+import ButtonConvDel from './Button.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Converter />
+      <ButtonConvDel/>
     </div>
   );
 }
