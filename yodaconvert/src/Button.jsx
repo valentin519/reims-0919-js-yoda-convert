d3b5ffffa32355aa136c61c555c8ff64a89af077
pablo <pmo83.mp@gmail.com>
2019-12-18 16:10:33 +0100

    navbar button translate reset made

---
+++ yodaconvert/src/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ButtonConvDel() {
+    return (
+      <div className='aeraButtons'>
+        <div className = 'convertButton'>
+          <button>Convertir</button>
+        </div>
+        <div className = 'resetButton'>
+          <button>Reset</button>
+        </div>
+      </div>
+    );
+  }
+
+export default ButtonConvDel;
