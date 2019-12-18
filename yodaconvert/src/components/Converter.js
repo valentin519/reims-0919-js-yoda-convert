df005b1139c6f77e88a9d63708f2ae0815e95c0e
Herve Bourelle <herve.bourelle@gmail.com>
2019-12-18 22:08:20 +0100

    add convert to html

--- yodaconvert/src/components/Converter.js
+++ yodaconvert/src/components/Converter.js
@@ -1,22 +1,36 @@
 import React, {Component} from "react";
 import '../App.css';
-
+import convertToHTML from 'markdown-to-html-converter' //npm install markdown-to-html-converter
 
 class Components extends Component {
     constructor (props) {
         super(props)
-        this.state={}
+        this.state={
+            inputText:'',
+            outputText:''
+        }
+        this.handleInputChange = this.handleInputChange.bind(this)
     }
+
+
+handleInputChange(e) {
+    this.setState({ 
+        inputText: e.target.value,
+        outputText : convertToHTML(this.state.inputText) })
+      }
+    
 render () {
+    console.log(this.state)
     return (
         <div className="textContainer">
-            <textarea className="input-text" rows="6" cols="40"></textarea>
-            <textarea className="output-text" rows="6" cols="40"></textarea>
+            
+            <textarea className="input-text" name="inputText" rows="6" cols="40" value={this.state.inputText} onChange={this.handleInputChange}>    
+            </textarea>
+            <textarea className="output-text" name="outputText" rows="6" cols="40" value={this.state.outputText} onChange={this.handleInputChange} >    
+            </textarea>
+             
         </div>
     )
 }}
-
-
-
 
 export default Components;
