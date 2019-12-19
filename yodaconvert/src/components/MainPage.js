609e72e5e37de0aa51515f2e8c08596017651766
Valentin JULIEN <valentinjulien93@gmail.com>
2019-12-19 21:50:24 +0100

    navbar created

--- yodaconvert/src/components/MainPage.js
+++ yodaconvert/src/components/MainPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import '../App.css';
 import Converter from './Converter.js'
+import NavBar from './NavBar.js'
 
 
 function MainPage(){
     return(
         <div className= "MainPage">
             <div className = "AsideBarre">
+                <NavBar/>
             </div>  
 
             <div className = "TittleBarre">
